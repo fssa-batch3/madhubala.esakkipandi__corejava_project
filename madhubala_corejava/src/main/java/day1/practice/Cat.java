89d2d3c270b8f72fc6cc36caa2825405b29e1ebf
Madhubala-06 <emadhubala1.@gmail.com>
2023-07-06 11:38:39 +0530

    practice update

--- madhubala_corejava/src/main/java/day1/practice/Cat.java
+++ madhubala_corejava/src/main/java/day1/practice/Cat.java
@@ -2,22 +2,25 @@ package day1.practice;
 
 public class Cat {
     // Attributes
-    private boolean hasDots;
+    private int age;
     private String color;
-
+    private String speak;
     // Constructor
-    public Cat(boolean hasDots, String color) {
-        this.hasDots = hasDots;
+    public Cat(int age, String color,String speak) {
+        this.age = age;
         this.color = color;
+        this.speak = speak;
     }
 
-    // Getters and setters
-    public boolean hasDots() {
-        return hasDots;
+   
+
+	// Getters and setters
+    public int age() {
+        return age;
     }
 
-    public void setHasDots(boolean hasDots) {
-        this.hasDots = hasDots;
+    public void setage(int age) {
+        this.age = age;
     }
 
     public String getColor() {
@@ -28,23 +31,34 @@ public class Cat {
         this.color = color;
     }
 
-		public void speak() {
-        System.out.println("Woof!");
+    
+    public String getspeak() {
+    	
+        return speak;
+        
+    }
+    
+   
+		public void getspeak(String speak) {
+			
+			
+			this.speak = speak;
     }
 
     // Main method to create Dog objects
     public static void main(String[] args) {
         // Create two Dog objects
-        Cat cat1 = new Cat(true, "purple");
-        Cat cat2 = new Cat(true, "blue");
+        Cat cat1 = new Cat(12, "purple","Meow!");
+        Cat cat2 = new Cat(14, "blue","Meow!");
 
         // Print the attributes of each Dog
         System.out.println("cat 1:");
-        System.out.println("Has Dots: " + cat1.hasDots());
+        System.out.println("Age " + cat1.age());
         System.out.println("Color: " + cat1.getColor());
-
+        System.out.println("Speaks: " + cat1.getspeak());
         System.out.println("cat 2:");
-        System.out.println("Has Dots: " + cat2.hasDots());
+        System.out.println("Age " + cat2.age());
         System.out.println("Color: " + cat2.getColor());
+        System.out.println("Speaks: " + cat2.getspeak());
     }
 }
