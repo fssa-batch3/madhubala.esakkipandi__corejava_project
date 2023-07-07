40fe1d4745f1b21e801b5b3031c79c2b83d4801a
Madhubala-06 <emadhubala1.@gmail.com>
2023-07-07 19:26:18 +0530

    comiting

--- madhubala_corejava/src/main/java/day1/practice/Cat.java
+++ madhubala_corejava/src/main/java/day1/practice/Cat.java
@@ -4,25 +4,61 @@ import java.util.Scanner;
 
 import java.util.Scanner;
 
-class Cat {
+public class Cat {
    
-   public static void main(String[] args) {
-       Scanner input = new Scanner(System.in);
+    private int Stripes;
+    private String color;
+    private String speaks;
 
-     
-       int n = input.nextInt();
+    public int getStripes() {
+        return Stripes;
+    }
 
-     
-       int[] array = new int[n];
+    public void setStripes(int Stripes) {
+        this.Stripes = Stripes;
+    }
 
- System.out.println(n);
-       int numOfInputs = input.nextInt();
-       for (int i = 0; i < numOfInputs; i++) {
-           array[i] = input.nextInt();
-       }
+    public String getColor() {
+        return color;
+    }
 
-       for (int i = 0; i < n; i++) {
-           System.out.println(array[i]);
-       }
-   }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    
+    public String setspeaks() {
+        return speaks;
+    }
+
+    public void setspeaks(String speaks) {
+        this.speaks = speaks;
+    }
+    
+
+    public static void main(String[] args) {
+      
+        Cat cat1 = new Cat();
+        cat1.setColor("White");
+        cat1.setStripes(4);
+        cat1.setspeaks("Meow!");
+        Cat cat2 = new Cat();
+        cat2.setColor("Black");
+        cat2.setStripes(3);
+        cat2.setspeaks("Meow!");
+
+        System.out.println("Cat 1:");
+        System.out.println("Age: " + cat1.getStripes());
+        System.out.println("Color: " + cat1.getColor());
+        System.out.println("Speaks: " + cat1.setspeaks());
+
+        System.out.println("Cat 2:");
+        System.out.println("Age: " + cat2.getStripes());
+        System.out.println("Color: " + cat2.getColor());
+        System.out.println("Speaks: " + cat2.setspeaks());
+    }
+    
 }
+    
+    
+    
+    
